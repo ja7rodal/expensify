c0fab03c4c0be566a8713106c4400c3cffd3491b
Jasson Rdz <ja7rodal@gmail.com>
2017-01-29 15:57:50 -0500

    create expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,29 @@
 class ExpensesController < ApplicationController
-  def index
-    @expenses = Expense.take(30)
-  end
+
+	def index
+		@expenses = Expense.where('date >= ? AND date <= ?', DateTime.now.beginning_of_month, DateTime.now).order(date: :desc )
+	end
+
+	def new
+		@expense = Expense.new
+	end
+
+	def create
+		@expense = Expense.create(expense_params)
+	end
+
+	def edit
+		@expense = Expense.find(params[:id])
+	end
+
+	def update
+		@expense = Expense.update(params[:id], expense_params)
+	end
+
+
+	private
+	def expense_params
+		params.require(:expense).permit(:concept, :value, :date, :type_expense, :category )
+	end
+
 end
